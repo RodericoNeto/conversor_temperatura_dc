a4e2be0e322787b7e8d5231510ad7ec77ce7db38
RodericoNeto <rodericocrneto@gmail.com>
2022-05-27 07:17:35 -0300

    adicionei a funcionalidade de mudar o botão de acordo com o option escolhido no select

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-alert("to funcionando")
-
-
-
+// FUNÇÃO CONVERTER A TEMPERATURA
 function convert(){
+
     let selectOption1 = document.getElementById("selectTemp1").value;
     let tempC = document.getElementById("tempC").value;
-
-    alert(`A temperatura que vc digitou é ${tempC}`)
 
     
     if (selectOption1 === "selecione"){
         alert("Por favor, escolha entre Farenheit ou Kelvin")
-    } else if (selectOption1 === "farenheit") {
+    } 
+    else if (selectOption1 === "farenheit") {
         let tempF = tempC*1.8 + 32;
-        alert(tempF)
         document.getElementById("result-F-or-K").innerHTML = `${tempF} ºF`;
-    } else {
+    } 
+    else {
         let tempK = tempC*1 + 273.15;
-        alert(tempK);
         document.getElementById("result-F-or-K").innerHTML = `${tempK} K`;
     }
 
+}
+
+
+// SELECIONAR E MUDAR O NOME DO BOTÃO PARA A TEMPERATURA DESEJADA
+// obs: deixar em caixa alta (toUpperCase())
+
+function changeButton() {
+    let tempSelected = document.getElementById("selectTemp1").value;
+    document.getElementById("buttonConvert").innerHTML = `CONVERTER PARA ${tempSelected.toUpperCase()}`
 }
